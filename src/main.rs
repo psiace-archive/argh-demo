38864f9fece5b79f3c82081f353987c46954bf41
Chojan Shang <psiace@outlook.com>
2020-02-07 22:36:02 +0800

    Basic add func with argh.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use argh::FromArgs;
+
+#[derive(FromArgs)]
+/// Add two numbers
+struct DemoCli {
+    /// the first number.
+    #[argh(option)]
+    num1: u16,
+
+    /// the second number
+    #[argh(option)]
+    num2: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli: DemoCli = argh::from_env();
+    add(cli.num1, cli.num2);
+}
+
+fn add(num1: u16, num2: u16) {
+    println!("{} + {} = {}", num1, num2, num1 + num2);
 }
