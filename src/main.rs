ccc26463f54a602913738c7ac18bcd107650eea5
Chojan Shang <psiace@outlook.com>
2020-02-25 17:22:29 +0800

    :beers: Change to subcommand.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,22 @@
 use argh::FromArgs;
 
 #[derive(FromArgs)]
-/// Add two numbers
+/// A simple calculation tool
 struct DemoCli {
+    #[argh(subcommand)]
+    subcommand: SubCommands,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum SubCommands {
+    Add(AddOptions),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Add two numbers
+#[argh(subcommand, name = "add")]
+pub struct AddOptions {
     /// the first number.
     #[argh(option)]
     num1: u16,
@@ -16,7 +30,11 @@ struct DemoCli {
 
 fn main() {
     let cli: DemoCli = argh::from_env();
-    add(cli.num1, cli.num2);
+    match cli.subcommand {
+        SubCommands::Add(options) => {
+            add(options.num1, options.num2);
+        }
+    };
 }
 
 fn add(num1: u16, num2: u16) {
